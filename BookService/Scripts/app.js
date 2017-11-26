9cf6183f21fb1707e1927ebc8fc77e0604ff1e55
colton <cgardner268@gmail.com>
2017-11-26 11:57:43 -0800

    Added abiltiy to add a new book and form to submit it on

--- BookService/Scripts/app.js
+++ BookService/Scripts/app.js
@@ -3,8 +3,17 @@
     self.books = ko.observableArray();
     self.error = ko.observable();
     self.detail = ko.observableArray();
+    self.authors = ko.observableArray();
+    self.newBook = {
+        Author: ko.observable(),
+        Genre: ko.observable(),
+        Price: ko.observable(),
+        Title: ko.observable(),
+        Year: ko.observable()
+    }
 
     var booksUri = '/api/books/';
+    var authorsUri = '/api/authors/';
 
     function ajaxHelper(uri, method, data) {
         self.error(''); // Clear error message
@@ -31,8 +40,30 @@
         });
     }
 
+    function getAuthors() {
+        ajaxHelper(authorsUri, 'GET').done(function (data) {
+            self.authors(data);
+        });
+    }
+
+    self.addBook = function (formElement) {
+        var book = {
+            AuthorId: self.newBook.Author().Id,
+            Genre: self.newBook.Genre(),
+            Price: self.newBook.Price(),
+            Title: self.newBook.Title(),
+            Year: self.newBook.Year()
+        };
+
+        ajaxHelper(booksUri, 'POST', book).done(function (item) {
+            self.books.push(item);
+        });
+
+    }
+
     // Fetch the initial data.
     getAllBooks();
+    getAuthors();
 };
 
 ko.applyBindings(new ViewModel());
